8a53a3fa071548ace2929302162fa1b501ef2421
dylan.tsou <dylan.tsou@vivicloud.net>
2021-11-16 15:31:35 +0800

    finished skill page

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../components/main/About";
 import Banner from "../components/main/Banner";
 import Skill from "../components/main/Skill";
+import Project from "../components/main/Project";
 
 const Main = () => {
 
@@ -10,6 +11,7 @@ const Main = () => {
     const home = document.querySelector(".home").offsetTop;
     const about = document.querySelector(".about").offsetTop;
     const skill = document.querySelector(".skill").offsetTop;
+    const project = document.querySelector(".project").offsetTop;
     const h = window.screen.height;
 
     if (top + h / 2 > home && top + h / 2 < about) {
@@ -22,6 +24,15 @@ const Main = () => {
     } else {
       document.querySelector(".about-nav").classList.remove("active");
     }
+    if (top + h / 2 > skill && top + h / 2 < project) {
+      document.querySelector(".skill-nav").classList.add("active");
+      document.querySelector(".skill-bar-value1").classList.add("active");
+      document.querySelector(".skill-bar-value2").classList.add("active");
+      document.querySelector(".skill-bar-value3").classList.add("active");
+      document.querySelector(".skill-bar-value4").classList.add("active");
+    } else {
+      document.querySelector(".skill-nav").classList.remove("active");
+    }
   });
 
   return (
@@ -29,6 +40,7 @@ const Main = () => {
       <Banner />
       <About />
       <Skill />
+      <Project />
     </>
   );
 };
