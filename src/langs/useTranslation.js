3eadd34d9b4b7c2f726f717e3574ea907d769fb2
dylan.tsou <dylan.tsou@vivicloud.net>
2021-12-15 13:55:24 +0800

    fix lang

--- src/langs/useTranslation.js
+++ src/langs/useTranslation.js
@@ -4,15 +4,11 @@ import { LanguageContext } from "./LanguageContext";
 export const useTranslation = (f) => {
   const languageContext = useContext(LanguageContext);
   const t = (text) => {
-    if (languageContext.dictionary && languageContext.dictionary[f]) {
-      return (
-        text
+    return languageContext.dictionary && languageContext.dictionary[f]
+      ? text
           .split(".")
           .reduce((o, i) => o && o[i], languageContext.dictionary[f]) || text
-      );
-    } else {
-      return text;
-    }
+      : text;
   };
 
   return { t };
